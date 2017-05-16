6c30572a013b54e2704b9863e5abcb54da7ce60c
Eugene Cherny <iam@oscii.ru>
2017-05-16 16:54:20 +0300

    [+] new podcast organization and episode filters

--- js/main.js
+++ js/main.js
@@ -1,35 +1,49 @@
-// var HEARTHIS_TEMPLATE = '<iframe scrolling="no" width="100%" height="145" src="https://hearthis.at/embed/%id%/" frameborder="0"></iframe>';
 var HEARTHIS_TEMPLATE = '<iframe scrolling="no" width="100%" height="150" src="https://hearthis.at/embed/%id%/transparent_black/?hcolor=&color=&style=2&block_size=1&block_space=1&background=1&waveform=0&cover=0&autoplay=0&css=" frameborder="0" allowtransparency></iframe>'
 var MIXCLOUD_TEMPLATE = '<iframe width="100%" height="120" src="https://www.mixcloud.com/widget/iframe/?feed=%id%&hide_cover=1" frameborder="0"></iframe>'
 
 $(function() {
-    $('.episode-content').addClass('hidden');
+    // Player 
+    $('#content > a.episode').click(function(e) {
+        e.preventDefault()
+        var episode = $(this)
+        var player = $('#player')
+        var playerType = episode.attr('data-player-type')
+        var playerData = episode.attr('data-player-data')
 
-    $('.episode > h2:first-child a').click(function(e) {
-        e.preventDefault();
-        var episode = $(this).parent().parent()
-        var episodeContent = episode.find('.episode-content').first();
-        var player = episode.find('.player').first();
-        var playerType = player.attr('data-player-type');
-
-        if (episodeContent.hasClass('hidden')) {
-            if (playerType == 'hearthis') {
-                player.html(HEARTHIS_TEMPLATE.replace('%id%', player.attr('data-hearthisid')));
-            } else if (playerType == 'mixcloud') {
-                player.html(MIXCLOUD_TEMPLATE.replace('%id%', player.attr('data-mixcloud')));
-            } else if (playerType == 'embed') {
-                player.html(unescape(player.attr('data-embed')));
-            }
-            episodeContent.removeClass('hidden');
+        if (playerType == 'hearthis') {
+            player.html(HEARTHIS_TEMPLATE.replace('%id%', playerData))
+        } else if (playerType == 'mixcloud') {
+            player.html(MIXCLOUD_TEMPLATE.replace('%id%', playerData))
         } else {
-            player.html('');
-            episodeContent.addClass('hidden');
+            console.log('Unknown player: ' + playerType)
         }
-    });
+        $('#content > a.episode.selected').removeClass('selected');
+        episode.addClass('selected');
+    })
+    $('#content > a.episode:first-child').click()
 
-    var podcastPMHourUTC = 17;
-    var timezoneOffset = parseInt((new Date()).getTimezoneOffset() * -1 / 60);
-    var timezoneSign = timezoneOffset < 0 ? '–' : '+';
+    // Filter
+    $('#episode-filter > a').click(function(e) {
+        e.preventDefault()
+        showEpisodeType = $(this).attr('data-show')
+        $('#content > a.episode').each(function() {
+            episodeType = $(this).attr('data-episode-type')
+            if (showEpisodeType === 'everything' || episodeType === showEpisodeType) {
+                $(this).show()
+            } else {
+                $(this).hide()
+            }
+        })
+
+        $('#episode-filter > a').removeClass('selected')
+        $(this).addClass('selected')
+    })
+    $('#episode-filter > a:first-child').click()
+
+    // Timezone
+    var podcastPMHourUTC = 17
+    var timezoneOffset = parseInt((new Date()).getTimezoneOffset() * -1 / 60)
+    var timezoneSign = timezoneOffset < 0 ? '–' : '+'
     var timeFormat = (podcastPMHourUTC + timezoneOffset) + ':00 (UTC' + timezoneSign + Math.abs(timezoneOffset) + ')'
-    $('#podcast-airtime').text(timeFormat);
-});
+    $('#podcast-airtime').text(timeFormat)
+})
